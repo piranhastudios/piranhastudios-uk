41d1f5b8a897d64904342a58b9177947321a6e20
jngatchu <jngatchu@gmail.com>
2024-10-10 11:27:10 +0100

    fixed allowTransparency error

--- src/components/data.js
+++ src/components/data.js
@@ -22,7 +22,8 @@ import {
 const benefitOne = {
     title: "Why Choose Us ",
     desc: "Choose Piranha Studios for unparalleled digital expertise and personalized service. We don't just build websites—we craft digital experiences that drive growth. Our team combines technical prowess with business insight to deliver solutions that truly work for you.",
-    image: <iframe width={521} height={521} allowTransparency className={"rounded-2xl"} src="https://lottie.host/embed/a45aecb6-8c8d-44af-b7a0-6e8d5f288ec8/huIqNpbmx7.json"></iframe>,
+    image: <iframe width={521} height={521} style={{backgroundColor: "transparent"}} className={"rounded-2xl"}
+                   src="https://lottie.host/embed/a45aecb6-8c8d-44af-b7a0-6e8d5f288ec8/huIqNpbmx7.json"></iframe>,
     bullets: [
         {
             title: "Expertise",
@@ -50,7 +51,8 @@ const benefitOne = {
 const benefitTwo = {
     title: "Our Approach",
     desc: "Our approach blends listening, design, and development to create more than just products—we&apos;re your committed digital partner. With Piranha Studios, turn your vision into a results-driven online presence.",
-    image: <iframe width={521} height={521} allowTransparency className={"rounded-2xl"} src="https://lottie.host/embed/5903e474-cb48-4877-abe5-a2ade8cddd05/ZeLNhobu0s.json"></iframe>,
+    image: <iframe width={521} height={521} style={{backgroundColor: "transparent"}} className={"rounded-2xl"}
+                   src="https://lottie.host/embed/5903e474-cb48-4877-abe5-a2ade8cddd05/ZeLNhobu0s.json"></iframe>,
     bullets: [
         {
             title: "Listen",
