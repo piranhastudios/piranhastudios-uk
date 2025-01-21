9597fa6c68e7ca90133ad707c7f05ee4266f7eb8
jakengatchu <jngatchu@gmail.com>
2025-01-21 21:47:31 +0000

    updating team section for princes trust

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -1,28 +1,30 @@
-import {Container} from "@/components/Container";
-import {Hero} from "@/components/Hero";
-import {SectionTitle} from "@/components/SectionTitle";
-import {Benefits} from "@/components/Benefits";
-import {Video} from "@/components/Video";
-import {Testimonials} from "@/components/Testimonials";
-import {Team as TeamList} from "@/components/Team";
-import {Cta} from "@/components/Cta";
-
-import {benefitOne, benefitTwo} from "@/components/data";
+// import {Container} from "@/components/Container";
+// import {Hero} from "@/components/Hero";
+// import {SectionTitle} from "@/components/SectionTitle";
+// import {Benefits} from "@/components/Benefits";
+// import {Video} from "@/components/Video";
+// import {Testimonials} from "@/components/Testimonials";
+// import {Team as TeamList} from "@/components/Team";
+// import {Cta} from "@/components/Cta";
+//
+// import {benefitOne, benefitTwo} from "@/components/data";
+import {redirect} from "next/navigation";
 
 export default function Team() {
-    return (
-        <Container>
-            <SectionTitle
-                preTitle="Our Team"
-                title="Wizards of The Web"
-            >
-                We empower businesses across diverse industries to revolutionize their technological approach. From
-                healthcare to industrial retail and restaurants, our tailored solutions drive innovation and efficiency,
-                helping our clients leverage technology to transform their operations and achieve remarkable growth.
-            </SectionTitle>
-
-            <TeamList/>
-            <Cta/>
-        </Container>
-    );
+    return redirect('/')
+    // (
+    //     <Container>
+    //         <SectionTitle
+    //             preTitle="Our Team"
+    //             title="Wizards of The Web"
+    //         >
+    //             We empower businesses across diverse industries to revolutionize their technological approach. From
+    //             healthcare to industrial retail and restaurants, our tailored solutions drive innovation and efficiency,
+    //             helping our clients leverage technology to transform their operations and achieve remarkable growth.
+    //         </SectionTitle>
+    //
+    //         <TeamList/>
+    //         <Cta/>
+    //     </Container>
+    // );
 }
